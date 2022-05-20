a6c96b12c97c09291c46eb9e43193a3b6d58247d
Mstrmind <104036615+Mstrmind-Hack@users.noreply.github.com>
2022-05-19 21:55:09 -0400

    Delete Breached directory

--- Breached/API_KEY.py
+++
@@ -1,4 +0,0 @@
-#This API Key is needed for https://leak-lookup.com/api/search the second "API". 
-#The API will respond with some web locations where the provided E-mail/Username was breached.
-
-API_KEY = ""
